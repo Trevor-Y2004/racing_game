d4b86b67e89671243e57f37881d6e45fe9962707
MicahM22 <mmiller5945@sdsu.edu>
2026-05-08 09:44:46 -0700

    Added MPH

--- Assets/Code/CarCode/car.cs
+++ Assets/Code/CarCode/car.cs
@@ -1,46 +1,53 @@
 using UnityEngine;
+using TMPro;
 
 public class car : MonoBehaviour
 {
     public Rigidbody rigid;
     public WheelCollider wheel1, wheel2, wheel3, wheel4;
     public float drivespeed, steerspeed;
+    public TextMeshProUGUI speedText;
+    public bool isGliding = false;
     float horizontalInput, verticalInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigid.centerOfMass = new Vector3(0, -0.3f, 0);
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        float speed = rigid.linearVelocity.magnitude * 2.237f;
+        speedText.text = Mathf.RoundToInt(speed) + " mph";
     }
 
     void FixedUpdate()
     {
-        //if player isn't pressing any vertical input controls, our vertical input is 0 so we don't drive
         float motor = Input.GetAxis("Vertical") * drivespeed;
 
         if (rigid.linearVelocity.magnitude < 5f)
         {
-        motor *= 2f;
+            motor *= 2f;
         }
 
         wheel1.motorTorque = 0;
         wheel2.motorTorque = 0;
         wheel3.motorTorque = motor;
         wheel4.motorTorque = motor;
-        //wheels turn at the wanted speed based on the horizontal input
-        wheel1.steerAngle = steerspeed * horizontalInput;
-        wheel2.steerAngle = steerspeed * horizontalInput;
 
-        Vector3 angularVel = rigid.angularVelocity;
-        angularVel.y *= 0.9f;
-        rigid.angularVelocity = angularVel;
+        float speed = rigid.linearVelocity.magnitude;
+        float steerLimit = Mathf.Lerp(steerspeed, 15f, speed / 30f);
+        wheel1.steerAngle = steerLimit * horizontalInput;
+        wheel2.steerAngle = steerLimit * horizontalInput;
+
+        if (!isGliding)
+        {
+            Vector3 angularVel = rigid.angularVelocity;
+            angularVel.y *= 0.9f;
+            rigid.angularVelocity = angularVel;
+        }
     }
 }
